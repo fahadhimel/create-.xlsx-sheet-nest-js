718e4c5c6aa730853e6865d64c647ee0e0f493fe
fahad himel <fahadhimel2@gmail.com>
2024-03-03 09:41:21 +0600

    swagger emplement done

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = 4005;
+
+  const config = new DocumentBuilder()
+    .setTitle('Exal Sheet')
+    .setDescription('The Exal Sheet API description')
+    .setVersion('1.0')
+    // .addTag('Exal Sheet')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(PORT);
+  // Logger
+  Logger.log(
+    `Server is Running(🔥) on http://127.0.0.1:${PORT}/api-doc/`,
+    'Exal sheet',
+  );
 }
 bootstrap();
